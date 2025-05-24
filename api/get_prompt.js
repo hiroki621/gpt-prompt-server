5f8d8554e830898ec1eaf6add2c1a00e5a1c1b7c
hiroki621 <145845833+hiroki621@users.noreply.github.com>
2025-05-24 17:42:34 +0900

    Add get_prompt.js

--- api/get_prompt.js
+++ api/get_prompt.js
@@ -3,7 +3,9 @@ export default async function handler(req, res) {
 
   const PROMPT_URLS = {
     prospect: 'https://gist.githubusercontent.com/hiroki621/cffe8d790344126ae4eef668d605290e/raw/Processor_Prospect_netkeibaStyle.txt',
-    recap: 'https://gist.githubusercontent.com/hiroki621/b2b4620eefaeae5765fcf3407ad142a3/raw/Processor_Recap.txt'
+    recap: 'https://gist.githubusercontent.com/hiroki621/b2b4620eefaeae5765fcf3407ad142a3/raw/Processor_Recap.txt',
+    racelog: 'https://gist.githubusercontent.com/hiroki621/fb055d7ff8413d292b15d5bfd0627bc5/raw/Processor_RaceLog.txt'
+
   };
 
   const url = PROMPT_URLS[type];
